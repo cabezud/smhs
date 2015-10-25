1ad61f99a4913dc18b5d14e550e8d2caf91c773c
cabezud <cabezud@hotmail.com>
2015-10-25 13:21:40 -0700

    style fixes

--- js/custom.js
+++ js/custom.js
@@ -36,9 +36,9 @@ $(function() {
       $('#tickets form .ss-text label').addClass('col-sm-3 control-label');
       $('#tickets form .ss-text input').wrap( "<div class='col-sm-9'></div>" );
       $('#tickets form .ss-text input').addClass('form-control');
-      $('#tickets form .ss-radio').addClass('col-sm-9 col-sm-offset-3')
-      //$('#tickets form input[value="Submit"]').addClass('col-sm-9 col-sm-offset-3')
-      $('#tickets form input[value="Submit"]').val("Continue to PayPal ($80) >>") ;
+      $('#tickets form .ss-radio').addClass('col-sm-9 col-sm-offset-3');
+      $('#tickets form .ss-navigate').addClass('clearfix');
+      $('#tickets form input[value="Submit"]').val("Continue to PayPal ($80) >>");
       //$('#tickets #entry_1985643611').removeAttr('style');//.css({'backgroun-image':'none','color':'green'});
       //$('#tickets #entry_1985643611').css({'background-image':'none','color':'green'});
       //$('#tickets form').css({'color':'green'});
