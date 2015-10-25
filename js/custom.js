d191cd311ca26571d24d9624285797941d99f89c
cabezud <cabezud@hotmail.com>
2015-10-25 12:59:30 -0700

    mostly form styling

--- js/custom.js
+++ js/custom.js
@@ -31,6 +31,18 @@ $(function() {
         $('.modal').modal('hide');
       });
 
+      $('#tickets form').addClass('form-horizontal');
+      $('#tickets form .ss-form-entry').addClass('form-group');
+      $('#tickets form .ss-text label').addClass('col-sm-3 control-label');
+      $('#tickets form .ss-text input').wrap( "<div class='col-sm-9'></div>" );
+      $('#tickets form .ss-text input').addClass('form-control');
+      $('#tickets form .ss-radio').addClass('col-sm-9 col-sm-offset-3')
+      //$('#tickets form input[value="Submit"]').addClass('col-sm-9 col-sm-offset-3')
+      $('#tickets form input[value="Submit"]').val("Continue to PayPal ($80) >>") ;
+      //$('#tickets #entry_1985643611').removeAttr('style');//.css({'backgroun-image':'none','color':'green'});
+      //$('#tickets #entry_1985643611').css({'background-image':'none','color':'green'});
+      //$('#tickets form').css({'color':'green'});
+
 });
 
 $(document).on('click','.navbar-collapse.in',function(e) {
